dc526e2edb93a9c4854e774888eada18d32bdfa6
will <will@wills-Mac-mini.local>
2022-11-05 11:12:34 +0700

    init

--- app/src/main/java/com/example/happyplaces/AddHappyActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyActivity.kt
@@ -2,10 +2,16 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy.*
 
 class AddHappyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy)
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }

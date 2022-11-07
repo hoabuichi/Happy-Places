b6c7c320ca0701c9b1bd0d470f23cb94e0e54b88
will <will@wills-Mac-mini.local>
2022-11-07 15:48:23 +0700

    add camera per

--- app/src/main/java/com/example/happyplaces/AddHappyActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyActivity.kt
@@ -1,17 +1,69 @@
 package com.example.happyplaces
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.internal.ManufacturerUtils
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddHappyActivity : AppCompatActivity(), View.OnClickListener {
 
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted. Continue the action or workflow in your
+            // app.
+            Toast.makeText(this@AddHappyActivity, "Granted 1", Toast.LENGTH_SHORT).show()
+        } else {
+            // Explain to the user that the feature is unavailable because the
+            // feature requires a permission that the user has denied. At the
+            // same time, respect the user's decision. Don't link to system
+            // settings in an effort to convince the user to change their
+            // decision.
+            Toast.makeText(this@AddHappyActivity, "Explain to the user that the feature is unavailable", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            val thumbnail: Bitmap = intent!!.extras!!.get("data") as Bitmap
+            iv_place_image.setImageBitmap(thumbnail)
+        }
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +84,8 @@ class AddHappyActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
+        btn_save.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -43,7 +97,113 @@ class AddHappyActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
             }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems) {
+                    _, which ->
+                    when(which) {
+                        0 -> {
+                            choosePhotoFromGallery()
+                        }
+
+                        1 -> {
+                            takePhoto()
+                        }
+                    }
+                }
+                pictureDialog.show()
+            }
+            R.id.btn_save -> {
+                Toast.makeText(this@AddHappyActivity, "Camera selection coming soon", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this)
+            .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddHappyActivity, "Storage READ/WRITE permissions are granted. Now you can select an image from gallery", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {
+                    showRationaleDialogForPermissions()
+                }
+            }).onSameThread().check()
+    }
+
+    private fun takePhoto() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startForResult.launch(intent)
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                    showRationaleDialogForPermissions()
+                    Toast.makeText(this@AddHappyActivity, "showRationaleDialogForPermissions 2", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startForResult.launch(intent)
+
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the feature requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    Toast.makeText(this@AddHappyActivity, "Explain to the user that the feature is unavailable 2", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage(" It looks like you have turned of permission required for this feature. I can be enabled under the Applications settings.")
+            .setPositiveButton("Go to Settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri: Uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+
     }
 
     private fun updateDateInView() {
